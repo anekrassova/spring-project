7c5f95f9ea26ea6ecc36b5844e9370861aa1782d
anekrassova <nni44561@gmail.com>
2024-09-10 23:14:09 +0500

    fixing displaying on localhost

--- src/main/java/com/example/springproject/SpringProjectApplication.java
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -2,6 +2,8 @@ package com.example.springproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringProjectApplication {
@@ -9,5 +11,13 @@ public class SpringProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringProjectApplication.class, args);
     }
+}
 
+@RestController
+class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
 }
